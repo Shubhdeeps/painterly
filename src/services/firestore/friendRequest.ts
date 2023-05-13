2879f605405ca9753af01d4f131fa6927edde0ad
Shubhdeep Singh <shubhdeep86993@gmail.com>
2023-05-13 16:23:19 +0300

    fixes

--- src/services/firestore/friendRequest.ts
+++ src/services/firestore/friendRequest.ts
@@ -2,7 +2,7 @@ import { FriendRequest } from "@/models/Notification";
 import { v4 as uuidv4 } from "uuid";
 import { auth, database, serverTimestamp } from "../firebaseConfig";
 
-export function sendFriendRequest(receiver: string) {
+export function sendFriendRequest(receiver: string, redirectLink: string) {
   const senderName = auth.currentUser?.displayName!;
   const content = `${senderName} sends you friend request.`;
   const id = uuidv4();
@@ -16,6 +16,7 @@ export function sendFriendRequest(receiver: string) {
     senderName,
     senderPhotoURL: auth.currentUser?.photoURL!,
     status: "unseen",
+    redirectLink,
   };
   try {
     database
